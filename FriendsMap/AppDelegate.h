bcc7880f959007a9a3a767eb73325c5e40e74beb
yuyedaidao <wyqpadding@gmail.com>
2015-03-04 10:22:15 +0800

    Initial Commit

---
+++ FriendsMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendsMap
+//
+//  Created by Wang on 15/3/4.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendsMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendsMap
+//
+//  Created by Wang on 15/3/4.
+//  Copyright (c) 2015年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
